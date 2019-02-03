80d5923c9811112005cd592ee16c520f75933b3a
agomcue486 <agomcue486@gmail.com>
2019-02-03 16:43:02 +0100

    Update do.,  3 de feb. de 2019 16:43:02

---
+++ virtualbox/guest-additions.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+apt-get -y update;
+apt-get -y upgrade;
+apt-get -y build-essential module-assistant;
+m-a -i prepare;
+mount /media/cdrom;
+cp -v /media/cdrom/VBoxLinuxAdditions.run /tmp;
+chmod 755 /tmp/VBoxLinuxAdditions.run;
+/tmp/VBoxLinuxAdditions.run;
+rm /tmp/VBoxLinuxAdditions.run;
