62c65177d68624614bc6d8b4aca6c6faf3a2421a
Alejandro G. <agomcue486@gmail.com>
2025-11-19 17:44:33 +0100

    cómo se instala virtualbox guest additions

--- virtualbox/guest-additions.sh
+++ virtualbox/guest-additions.sh
@@ -5,7 +5,5 @@ apt-get -y upgrade;
 apt-get -y install build-essential module-assistant;
 m-a -i prepare;
 mount /media/cdrom;
-cp -v /media/cdrom/VBoxLinuxAdditions.run /tmp;
-chmod 755 /tmp/VBoxLinuxAdditions.run;
-/tmp/VBoxLinuxAdditions.run;
-rm /tmp/VBoxLinuxAdditions.run;
+./VBoxLinuxAdditions.run;
+
